1bf67f8c4c7081611783763a7f982bbbc31ee27f
Barış Demirci <barbarbar338@picass.co>
2020-06-23 08:26:25 +0000

    Code cleaned and examples fixed

--- test.js
+++ test.js
@@ -1,12 +1,5 @@
-# pretendre
-
-A lightweight package to send POST and GET requests easy and fast.
-
-
-Example:
-```js
-const pretendre = require("pretendre");
-
+const pretendre = require(".");
+ 
 // To send a GET request ->
 pretendre.makeRequest.get("https://ptsv2.com/t/8unj2-1592899008#/")
     .then((res) => {
@@ -26,6 +19,3 @@ pretendre.makeRequest.post("https://ptsv2.com/t/8unj2-1592899008/post")
     .catch((err) => {
         // handle error
     });
-```
-
-For any problem, create a request on Github, click [here](https://github.com/snowy1000/pretendre) (just to clarify, using Github is a better way), or join our Discord server by clicking [here](https://discord.gg/JajWkmj).
